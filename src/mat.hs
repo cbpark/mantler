b8b1fe8345d2d1081d908310f41bfa735feb6012
cbpark <cbpark@gmail.com>
2020-04-28 21:28:29 +0900

    Try (with leptons?)

--- src/mat.hs
+++ src/mat.hs
@@ -18,12 +18,15 @@ main = do
     events <- decompress <$> B.readFile lheFile
     runEffect $ getLHEFEvent fromLazy events
         >-> P.map (getSAT 80.379 173.0)
+        -- >-> P.map (getSAT' 0 173.0)
         >-> P.print
+
+pH' :: FourMomentum
+pH' = setXYZT 0 0 0 800
 
 getSAT :: Double -> Double -> Event -> Maybe (Double, Double)
 getSAT m0 m1 ps = do
     (pH, pBs) <- selectP ps
-    let pH' = setXYZT 0 0 0 800
     at <- mkAntler m0 m1 (visibles pBs)
     return (sAT at pH, sAT at pH')
 
@@ -38,3 +41,22 @@ selectP ev = do
   where
     topQuarks = ParticleType [6]
     isBquark = (== 5) . abs . idOf
+
+-- getSAT' :: Double -> Double -> Event -> Maybe (Double, Double)
+-- getSAT' m0 m1 ps = do
+--     (pH, pBs) <- selectP' ps
+--     at <- mkAntler m0 m1 (visibles pBs)
+--     return (sAT at pH, sAT at pH')
+
+-- selectP' :: Event -> Maybe (FourMomentum, [FourMomentum])
+-- selectP' ev = do
+--     let topChild = particlesFrom topQuarks (eventEntry ev)
+--     if null topChild
+--         then Nothing
+--         else do let pH = momentumSum $ fourMomentum <$> concat topChild
+--                     pV = momentumSum . fmap fourMomentum <$>
+--                         (filter (not . isNeutrino) <$> topChild)
+--                 return (pH, pV)
+--   where
+--     topQuarks = ParticleType [6]
+--     isNeutrino = (`elem` neutrinos) . idOf
