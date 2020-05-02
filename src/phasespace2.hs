d9772800e18653ab9aaf9e69e652121198a23312
cbpark <cbpark@gmail.com>
2020-05-02 20:49:40 +0900

    Add phasespace2.hs

--- src/phasespace2.hs
+++ src/phasespace2.hs
@@ -8,26 +8,25 @@ import           MAT.Helper
 
 import           HEP.Data.LHEF
 
-import           Codec.Compression.GZip            (decompress)
-import qualified Data.ByteString.Char8             as C
-import           Data.ByteString.Lazy.Char8        (ByteString)
-import qualified Data.ByteString.Lazy.Char8        as BL
-import           Data.Double.Conversion.ByteString (toExponential)
+import           Codec.Compression.GZip     (decompress)
+import qualified Data.ByteString.Char8      as C
+import           Data.ByteString.Lazy.Char8 (ByteString)
+import qualified Data.ByteString.Lazy.Char8 as BL
 
 import           Pipes
-import           Pipes.ByteString                  (fromLazy)
-import qualified Pipes.Prelude                     as P
+import           Pipes.ByteString           (fromLazy)
+import qualified Pipes.Prelude              as P
 
-import           Control.Monad                     (forever, unless)
-import           System.Environment                (getArgs)
-import           System.Exit                       (die)
+import           Control.Monad              (forever, unless)
+import           System.Environment         (getArgs)
+import           System.Exit                (die)
 import           System.IO
 
 main :: IO ()
 main = do
     args <- getArgs
     unless (length args == 2) $
-        die "-- Usage: phasespace1 <LHEF file gzipped> <output>"
+        die "-- Usage: phasespace2 <LHEF file gzipped> <output>"
 
     let lheFile = head args
     putStrLn $ "-- The input LHEF file is " <> lheFile <> "."
@@ -44,21 +43,19 @@ main = do
 
     putStrLn $ "-- ... Done!\n" <> "-- " <> outfile <> " has been generated."
 
-data Var = Var { _deltaAT1 :: !Double -- ^ the AT variable for M2 = M2(true) / 2
-               , _deltaAT2 :: !Double -- ^ the AT variable for M2 = 2 * M2(true)
-               , _atTrue   :: !AT  -- ^ the AT variabless for M2 = M2(true)
+data Var = Var { _at0 :: !AT  -- ^ the AT variables for p_T = 0
+               , _at1 :: !AT  -- ^ the AT variables for p_T = p_T^{true}
                } deriving Show
 
 calcVar :: Double -> Double -> Double -> Event -> Maybe Var
 calcVar m0 m1 m2 ps = do
-    (_, pBs) <- selectP ps
+    (pH, pBs) <- selectP ps
     at <- mkAntler m0 m1 (visibles pBs)
-    let deltaATval1 = deltaAT at 0 0 0 (m2 / 2)
-        deltaATval2 = deltaAT at 0 0 0 (2 * m2)
-        atTrue = calcAT at 0 0 0 m2
-    return $ Var { _deltaAT1 = deltaATval1
-                 , _deltaAT2 = deltaATval2
-                 , _atTrue   = atTrue }
+    let qx = px pH
+        qy = py pH
+        at0 = calcAT at 0  0  0 m2
+        at1 = calcAT at qx qy 0 (sqrt $ m2 * m2 + qx * qx + qy * qy)
+    return $ Var at0 at1
 
 selectP :: Event -> Maybe (FourMomentum, [FourMomentum])
 selectP ev = do
@@ -78,13 +75,11 @@ printVar h = forever $ do
     liftIO $
         case vars of
             Nothing       -> hPutStrLn stderr "failed!"
-            Just Var {..} -> C.hPutStrLn h $
-                toExponential 8 _deltaAT1 <> "  " <> toExponential 8 _deltaAT2
-                <> "  " <> showAT _atTrue
+            Just Var {..} -> C.hPutStrLn h $ showAT _at0 <> "  " <> showAT _at1
 
 header :: ByteString
 header = BL.pack $ "# " <>
          foldl1 (\v1 v2 -> v1 <> ", " <> v2)
          (zipWith (\n v -> "(" <> show n <> ") " <> v) ([1..] :: [Int])
-             [ "deltaAT(M2/2)", "deltaAT(2*M2)"
-             , "deltaAT(true)", "mAT(min)", "mAT(max)" ])
+             [ "deltaAT(0)",  "mAT(min, 0)",  "mAT(max, 0)"
+             , "deltaAT(pT)", "mAT(min, pT)", "mAT(max, pT)" ])
